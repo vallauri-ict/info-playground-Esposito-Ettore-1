4b1d9b1b73da180205109e82e0e294e3550b75e3
unknown <e.esposito.1520@vallauri.edu>
2021-09-21 17:19:33 +0200

    Esercizio 2

---
+++ 01_02_Aereoporto/01_02_Aereoporto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Aereoporto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 01_02_Aereoporto/README.md
@@ -0,0 +1,3 @@
+# Esposito Ettore
+## Aereoporto
+Creazione di un database con tre tabelle per gli aereoporti, i voli e i tipi di aereo.
