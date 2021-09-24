4c87f4e523bc2df5e001e8c8b87df9679b9a7d7f
unknown <e.esposito.1520@vallauri.edu>
2021-09-24 10:49:40 +0200

    Select esercizio 2

---
+++ 01_02_Aereoporto/Select.sql
@@ -0,0 +1,14 @@
+﻿/* Città da cui si può partire per Roma */
+SELECT DISTINCT cittaPartenza FROM Volo 
+WHERE cittaArrivo = 'Roma' 
+ORDER BY cittaPartenza
+
+/* Aereoporti in cui non è noto il numero di piste */
+SELECT citta, nazione FROM Aereoporto 
+WHERE numPiste IS NULL 
+ORDER BY nazione, citta
+
+/* Numero di passeggeri e di merci per un volo */
+SELECT v.idVolo, a.tipoAereo, a.numPasseggeri, a.qtaMerci FROM Volo v, Aereo a 
+WHERE v.tipoAereo = a.tipoAereo AND a.qtaMerci > 0 AND a.numPasseggeri > 0
+ORDER BY 1
