88540468ccb4bb0ab99acabea67ec602fd6bdb0a
unknown <e.esposito.1520@vallauri.edu>
2021-09-25 13:07:54 +0200

    Select esercizio 3

---
+++ 01_03_Cinema/Insert.sql
@@ -0,0 +1,50 @@
+﻿/* Attori */
+INSERT INTO Attore VALUES ('Tizio', 1980, 'IT')
+
+INSERT INTO Attore VALUES ('Caio', 1972, 'RM')
+
+INSERT INTO Attore VALUES ('Sempronio', 1999, 'GB')
+
+
+/* Film */
+INSERT INTO Film VALUES ('Star Wars', 2019, 'FR', 'F. Fellini', 'Fantascienza', 130)
+
+INSERT INTO Film VALUES ('Lord of the ring', 2012, 'IT', 'S. Spielberg', 'Fantasy', 160)
+
+INSERT INTO Film VALUES ('IT', 2019, 'JP', 'S. Spielberg', 'Horror', 122)
+
+INSERT INTO Film VALUES ('Casablanca', 1990, 'JP', 'S. Spielberg', 'Fantascienza', 180)
+
+
+
+/* Sale */
+INSERT INTO Sala VALUES (200, 'Sala A', 'Pisa')
+
+INSERT INTO Sala VALUES (130, 'Sala B', 'Pisa')
+
+INSERT INTO Sala VALUES (250, 'Sala C', 'Milano')
+
+
+/* Recite */
+INSERT INTO Recita VALUES (1, 1)
+INSERT INTO Recita VALUES (1, 2)
+INSERT INTO Recita VALUES (1, 3)
+
+INSERT INTO Recita VALUES (2, 2)
+INSERT INTO Recita VALUES (2, 3)
+
+INSERT INTO Recita VALUES (3, 3)
+
+
+/* Proiezioni */
+INSERT INTO Proiezione VALUES (1, 1, 160.33, '2021-8-4')
+INSERT INTO Proiezione VALUES (1, 2, 260.36, '2021-8-5')
+INSERT INTO Proiezione VALUES (1, 3, 120.00, '2021-9-20')
+
+INSERT INTO Proiezione VALUES (2, 1, 130.22, '2021-9-15')
+INSERT INTO Proiezione VALUES (2, 3, 15.80, '2021-10-4')
+
+INSERT INTO Proiezione VALUES (3, 3, 600.83, '2021-11-1')
+
+
+
---
+++ 01_03_Cinema/Select.sql
@@ -0,0 +1,59 @@
+﻿SELECT Nome FROM Sala 
+WHERE Citta = 'Pisa'
+
+SELECT Titolo FROM Film 
+WHERE Regista = 'F. Fellini' AND AnnoProduzione > 1960
+ORDER BY AnnoProduzione DESC
+
+SELECT Titolo, Durata FROM Film 
+WHERE (Genere = 'Fantascienza' AND Nazionalita = 'JP') OR (Nazionalita = 'FR' AND AnnoProduzione > 1990)
+ORDER BY Nazionalita, AnnoProduzione DESC
+
+SELECT Titolo FROM Film
+WHERE (Nazionalita = 'JP' AND Genere = 'Fantascienza' AND AnnoProduzione > 1990) OR Nazionalita = 'FR'
+ORDER BY Nazionalita, AnnoProduzione DESC
+
+SELECT f1.Titolo, f1.Regista FROM Film f1
+WHERE f1.Regista IN (SELECT DISTINCT f2.Regista FROM Film f2 WHERE f2.Titolo = 'Casablanca')
+ORDER BY 2, 1
+
+SELECT DISTINCT f.Titolo, f.Genere FROM Proiezione p, Film f
+WHERE f.CodFilm = p.CodFilm AND p.DataProiezione = '2004-12.25'
+ORDER BY 1
+
+SELECT DISTINCT f.Titolo, f.Genere FROM Proiezione p, Film f, Sala s
+WHERE f.CodFilm = p.CodFilm AND p.DataProiezione = '2004-12.25' AND s.CodSala = p.CodSala AND s.Citta = 'Napoli'
+ORDER BY 1
+
+SELECT DISTINCT s.Nome FROM Sala s, Proiezione p, Attore a, Recita r
+WHERE (s.CodSala = p.CodSala AND p.CodFilm = r.CodFilm AND r.CodAttore = a.CodAttore)
+AND (p.DataProiezione = '2004-12-25' AND a.Nome = 'R. Williams' AND s.Citta = 'Napoli')
+ORDER BY 1
+
+SELECT DISTINCT f.Titolo FROM Film f, Attore a, Recita r
+WHERE (r.CodAttore = a.CodAttore AND r.CodFilm = f.CodFilm)
+AND a.Nome IN ('Tizio', 'Caio')
+ORDER BY 1
+
+/*10*/
+SELECT f.titolo FROM Film f, Attore a1, Recita r1, Attore a2, Recita r2
+WHERE (f.CodFilm = r1.CodFilm AND f.CodFilm = r2.CodFilm AND r1.CodAttore = a1.CodAttore AND r2.CodAttore = a2.CodAttore)
+AND a1.Nome = 'Tizio' AND A2.Nome = 'Caio'
+ORDER BY 1
+
+/*10*/
+SELECT f.titolo FROM Film f, Attore a, Recita r 
+WHERE (f.CodFilm = r.CodFilm AND r.CodAttore = a.CodAttore AND a.Nome = 'Tizio') 
+AND f.CodFilm IN (SELECT f2.CodFilm FROM Film f2, Attore a2, Recita r2 
+	WHERE f2.CodFilm = r2.CodFilm AND r2.CodAttore = a2.CodAttore AND a2.Nome = 'Caio')
+ORDER BY 1
+
+SELECT f.Titolo, a.Nome FROM Film f, Attore a, Recita r
+WHERE f.CodFilm = r.CodFilm AND r.CodAttore = a.CodAttore
+AND a.Nazionalita = 'FR'
+ORDER BY 1, 2
+
+SELECT f.Titolo, s.Sala FROM Film f, Sala s, Proiezione p
+WHERE f.CodFilm = p.CodFilm AND p.CodSala = s.CodSala
+AND p.DataProiezione BETWEEN '2005-01-01' AND '2005-01-31' and s.Citta = 'Pisa'
+ORDER BY 2, 1
