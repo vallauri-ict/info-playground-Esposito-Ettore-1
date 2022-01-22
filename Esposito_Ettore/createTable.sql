008de9f50758c75170bc70262f69f08d185b79f4
Stud. Ettore Esposito <e.esposito.1520@vallauri.net>
2022-01-22 13:11:58 +0100

    join

---
+++ Esposito_Ettore/createTable.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE Attori (
+	CodAttore int not null primary key identity(1, 1),
+	Nome varchar(30) not null,
+	AnnoNascita smallint not null,
+	Nazionalita varchar(3) not null
+);
+
+CREATE TABLE Film (
+	CodFilm int not null primary key identity(1, 1),
+	Titolo varchar(30) not null,
+	AnnoProduzione smallint not null,
+	Nazionalita varchar(3) not null,
+	Regista varchar(30) not null,
+	Genere varchar(20) not null
+);
+
+CREATE TABLE Sale (
+	CodSala int not null primary key identity(1, 1),
+	Posti int not null,
+	Nome varchar(30) not null,
+	Citta varchar(30) not null
+);
+
+CREATE TABLE Recita (
+	CodAttore int not null,
+	CodFilm int not null,
+	primary key (CodAttore, CodFilm),
+	foreign key (CodAttore) references Attori(CodAttore),
+	foreign key (CodFilm) references Film(CodFilm)
+);
+
+CREATE TABLE Proiezioni (
+	CodProiezione int not null primary key identity(1, 1),
+	CodFilm int not null,
+	CodSala int not null,
+	Incasso decimal(6, 2) not null,
+	DataProiezione date not null,
+	foreign key (CodFilm) references Film(CodFilm),
+	foreign key (CodSala) references Sale(CodSala)
+);
---
+++ Esposito_Ettore/select.sql
@@ -0,0 +1,26 @@
+﻿/* 1 */
+SELECT f.Titolo FROM Film f
+WHERE f.Regista = (SELECT Regista FROM Film f2 WHERE f2.Titolo = 'Casablanca');
+
+/* 2 */
+SELECT DISTINCT f.Titolo FROM Attori a, Film f, Recita r
+WHERE a.CodAttore = r.CodAttore AND f.CodFilm = r.CodFilm
+AND a.Nome IN ('M.Mastroianni', 'S.Loren');
+
+/* 3 */
+SELECT DISTINCT f.Titolo, f.Genere FROM Film f, Proiezioni p
+WHERE f.CodFilm = p.CodFilm
+AND p.DataProiezione = '2004-12-25';
+
+/* 4 */
+SELECT f.Titolo, f.Regista FROM Film f
+WHERE (SELECT COUNT(*) FROM Attori a, Recita r WHERE a.CodAttore = r.CodAttore AND f.CodFilm = r.CodFilm) < 6;
+
+/* 5 */
+SELECT f.Titolo from Film f
+WHERE NOT EXISTS(SELECT * FROM Sale s, Proiezioni p WHERE s.CodSala = p.CodSala AND f.CodFilm = p.CodFilm AND s.Citta = 'Pisa');
+
+/* 6 */
+SELECT a.Nome FROM Attori a
+WHERE NOT EXISTS(SELECT * FROM Film f, Recita r WHERE f.CodFilm = r.CodFilm AND a.CodAttore = r.CodAttore AND f.Regista = 'Fellini')
+AND a.Nazionalita = 'IT';
