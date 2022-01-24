ed7633b3aa43545c5e3cf7bca73c17a29191488e
Stud. Ettore Esposito <e.esposito.1520@vallauri.net>
2022-01-24 13:00:21 +0100

    join, union, intersect e except

---
+++ 01_07_Union-Intersect-Minus/01_07_Union-Intersect-Minus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_07_Union_Intersect_Minus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
