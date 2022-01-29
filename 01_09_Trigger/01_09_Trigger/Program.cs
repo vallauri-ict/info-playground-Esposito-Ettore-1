99450b10384223cf20cf9e55d3250fcfddb163b0
Stud. Ettore Esposito <e.esposito.1520@vallauri.net>
2022-01-29 12:31:15 +0100

    Trigger

---
+++ 01_09_Trigger/01_09_Trigger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_09_Trigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
