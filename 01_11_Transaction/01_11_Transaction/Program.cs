b0fbec445bb791130a7f48b05bd571d3fd0dae40
Stud. Ettore Esposito <e.esposito.1520@vallauri.net>
2022-02-05 11:40:11 +0100

    Transaction

---
+++ 01_11_Transaction/01_11_Transaction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_11_Transaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 01_11_Transaction/Transaction.sql
@@ -0,0 +1,17 @@
+﻿BEGIN TRANSACTION;
+
+	INSERT INTO Anagrafica(Nome, Stipendio) VALUES ('Neri', 35000);
+	SAVE TRANSACTION SavePoint1;
+
+	INSERT INTO Anagrafica(Nome, Stipendio) VALUES ('Viola', 35000);
+	SAVE TRANSACTION SavePoint2;
+
+	INSERT INTO Anagrafica(Nome, Stipendio) VALUES ('Marrone', 25000);
+
+	SELECT * FROM Anagrafica; /* Con Marrone */
+
+	ROLLBACK TRANSACTION SavePoint2;
+
+	SELECT * FROM Anagrafica; /* Senza Marrone */
+
+COMMIT;
